9067bb8edb35baf7d79e93751c0ef84e6003f874
Yatagarassu9 <rafaelyt47@gmail.com>
2026-05-28 21:17:11 -0300

    agregando Sucursales

--- Backend/src/models/appointment.js
+++ Backend/src/models/appointment.js
@@ -1,12 +1,22 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../db.js";
-
+import { BRANCHES } from "../enums/enums.js";
 const Appointment = sequelize.define("Appointment", {
 
   appointmentDate: {
     type: DataTypes.DATE,
     allowNull: false
   },
+
+branch: {//sucursales
+  type: DataTypes.ENUM(
+    BRANCHES.ARROYITO,
+    BRANCHES.PICHINCHA,
+    BRANCHES.BARRIO_MARTIN
+  ),
+  allowNull: false
+},
+
 
   status: {
     type: DataTypes.ENUM(
