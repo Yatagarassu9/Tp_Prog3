2db9102316d7160e45e4a0c8eb7186de649688cf
Yatagarassu9 <rafaelyt47@gmail.com>
2026-06-22 20:09:51 -0300

    add responsive+fixing errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,6 @@ import ChangePasswordPage from "./pages/client/ChangePasswordPage";
 import AdminDashboardPage from "./pages/admin/AdminDashboardPage";
 import AdminManagePage from "./pages/admin/AdminManagePage";
 import NotFoundPage from "./pages/public/NotFoundPage";
-import AdminDashboardPage from "./pages/admin/AdminDashboardPage";
-import AdminManagePage from "./pages/admin/AdminManagePage";
 import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Routes, Route, Navigate, Outlet, useLocation } from "react-router";
 import { AuthProvider, useAuth } from "./context/AuthContext";
@@ -63,11 +61,6 @@ function App() {
                 <Route index element={<AdminDashboardPage />} />
                 <Route path="manage" element={<AdminManagePage />} />
               </Route>
-            </Route>
-
-            <Route path="/admin">
-              <Route index element={<AdminDashboardPage />} />
-              <Route path="manage" element={<AdminManagePage />} />
             </Route>
           </Route>
           <Route path="*" element={<NotFoundPage />} />
